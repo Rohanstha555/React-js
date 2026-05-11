d66c7d3191c7ffadf7fb6604bbff8b4a65a3aadb
MinatoSensei <stharohan00@gmail.com>
2026-05-11 11:10:07 +0545

    feat: update product fetching logic and improve product display in Card component

--- intern/src/components/product-card/card.jsx
+++ intern/src/components/product-card/card.jsx
@@ -3,35 +3,45 @@ import { useState, useEffect } from "react";
 function Card() {
   const [product, setProduct] = useState([]);
 
+const fetchproduct = async () => {
+        const res = await fetch("http://localhost:4000/product/products")
+
+      const data = await res.json()
+
+     
+      const products = data.products
+      console.log(products);
+      
+      setProduct(products)
+}
   useEffect(() => {
-    fetch("https://fakestoreapi.com/products")
-      .then((res) => res.json())
-      .then((data) => setProduct(data))
-      .catch((error) => console.error("Error fetching products:", error));
-    // console.log(data)
+      
+fetchproduct()
   }, []);
+
   console.log(product);
   return (
     <div className="flex flex-wrap gap-4 p-4 justify-center">
       {product.map((item) => (
         <div
-          key={item.id}
+        
           className="w-48 bg-white border border-gray-100 rounded-xl overflow-hidden"
         >
           <div className="bg-gray-50 h-36 flex items-center justify-center">
             <img
-              src={item.image}
-              alt={item.title}
+              src={item.productImg}
+              alt={item.product_name
+}
               className="h-20 object-contain"
             />
           </div>
 
           <div className="p-3">
             <p className="text-sm font-semibold text-gray-900 mb-1 truncate">
-              {item.title}
+              {item.product_name}
             </p>
             <p className="text-xs text-gray-400 mb-3 truncate">
-              {item.category}
+              {item.category.name}
             </p>
             <div className="flex items-center justify-between">
               <span className="text-sm font-bold text-gray-900">
